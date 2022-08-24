fdb9ade15c3a8c8ac924f488ca815f4db96d3c18
Gabriel Silva <gabriel127.gk@gmail.com>
2022-08-24 04:12:52 -0300

    Add 2 tags br no formLogin.

--- src/shared/components/forms/formLogin/index.tsx
+++ src/shared/components/forms/formLogin/index.tsx
@@ -11,36 +11,39 @@ import { FormContainer, OptionsContainer } from "./styles";
 export const FormLogin = () => {
 
   return (
-    <FormContainer onSubmit={() => {}}
-      variants={container}
-      initial="hidden"
-      animate="visible"
-    >
-      <motion.div variants={item}>
-        <SingleInput>
-          <InputMask mask="999.999.999-99" type="text" id="cpf" 
-            className="input" 
-            required 
-            />
-          <label htmlFor="cpf">CPF</label>
-        </SingleInput>
+    <>
+        <br /><br />
+        <FormContainer onSubmit={() => {}}
+          variants={container}
+          initial="hidden"
+          animate="visible"
+        >
+            <motion.div variants={item}>
+              <SingleInput>
+                <InputMask mask="999.999.999-99" type="text" id="cpf" 
+                  className="input" 
+                  required 
+                  />
+                <label htmlFor="cpf">CPF</label>
+              </SingleInput>
 
-        <SingleInput>
-          <input type="password" id="password" 
-            className="input" placeholder="Senha" 
-            required 
-            />
-          <label htmlFor="password">Senha</label>
-        </SingleInput>
-        <OptionsContainer>
-          <p>Ainda não é cadastrado?</p>
-          <p>Esqueceu sua senha?</p>
-        </OptionsContainer>
+              <SingleInput>
+                <input type="password" id="password" 
+                  className="input" placeholder="Senha" 
+                  required 
+                  />
+                <label htmlFor="password">Senha</label>
+              </SingleInput>
+              <OptionsContainer>
+                <p>Ainda não é cadastrado?</p>
+                <p>Esqueceu sua senha?</p>
+              </OptionsContainer>
 
-        <ButtonsStyle>
-          <button type="submit">ENTRAR</button>
-        </ButtonsStyle>
-      </motion.div>
-    </FormContainer>
+              <ButtonsStyle>
+                <button type="submit">ENTRAR</button>
+              </ButtonsStyle>
+            </motion.div>
+        </FormContainer>
+    </>
   );
 };
